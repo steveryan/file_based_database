a4e35a172d910a2cb28b46f869c765fa7bad00da
steveryan <steveryan6989@gmail.com>
2019-04-25 22:53:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Add Email
+Make users unique by Email
+Add `User#update(first_name, last_name, email)`
+Add `User#destroy`
+Add `User.destroy_all`
---
+++ user.rb
@@ -0,0 +1,47 @@
+require "pry"
+
+class User
+  attr_accessor :first_name, :last_name
+
+  def self.create(first_name:, last_name:)
+    new_user = new(
+      first_name: first_name,
+      last_name: last_name
+    )
+    users = self.all.map(&:to_h) << new_user.to_h
+    File.write("users.json", users.to_json)
+    new_user
+  end
+
+  def self.all
+    users_json = File.exist?("users.json") ? File.read("users.json") : "[]"
+    parsed_users = JSON.parse(users_json, symbolize_names: true)
+    parsed_users.map do |user|
+      User.new(first_name: user[:first_name], last_name: user[:last_name])
+    end
+  end
+
+  def initialize(first_name:, last_name:)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def to_s
+    "#{@first_name} #{@last_name}"
+  end
+
+  def full_name
+    to_s
+  end
+
+  def to_h
+    {first_name: first_name, last_name: last_name}
+  end
+
+  def to_json
+    to_h.to_json
+  end
+end
+
+brooks = User.create(first_name: "Brooks", last_name: "Swinnerton")
+steve = User.create(first_name: "Steve ", last_name: "Ryan")
